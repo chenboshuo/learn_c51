32568135195d61dc3158fe7fd7a4ebc5171f2d85
chenboshuo <chenbohuo.hi@163.com>
2021-04-28 19:46:45 +0800

    :art: add alias

--- myheader.h
+++ myheader.h
@@ -4,6 +4,7 @@
  * 方便代码的理解
  */
 #include <reg52.h>
+
 // 寄存器别名
 #define DATA P0  // P0 寄存器一般传递数据，为了阅读方便，改名为 DATA
 #define ADDRESS \
@@ -18,6 +19,10 @@
 #define CLOSE_ALL 0xff        // 所有位为 1 表示全部关闭
 #define ENABLE_LED_ARRAYS 14  // 01110 表示右边LED单独的led灯
 #define TRUE 1
+#define ENALBE 1
+
+// 计数器的初值
+#define TIME_50_MS 0x4c00
 
 /**
  * DIGITS_LED[i] 0<=i<16 表示i的数码管的表示
@@ -52,31 +57,28 @@ unsigned char code DIGITS_LED[] = {
  */
 void keep_one_second() {
   char count = 0;  // 记录T0中断次数
-  TMOD = 0x10;  // 设置T0为模式1 TMOD 高四位控制T1 GATE=0 T/C = 0 (M1 M0) = 1
-                // 工作方式 模式1
-
-  T1 = 0x4c00;
-  /*
-   * TL1 = 0x00;
-   * TH1 = 0xB8;  // 为T0赋初值0xB800, 根据主频计算计时20ms的初值
-   * 以下是计时20ms 的初值，t20_two = 0xB800
-   * t20 = 2^16 - t20_two
-   * t50 = t20/2 * 5
-   * t50_two = 2^16 - t50 = 19456
-   * TH0 = 0xB8;  // 重新赋初值
-   * TL0 = 0x00;
-   */
-  TR1 = 1;  // 启动T1
+  TMOD = 0x10;  // 设置T1为模式1 TMOD 高四位控制T0 GATE=0 T/C = 0 (M1 M0) = 1
+                // 工作方式0
+  // TH1 = 0xB8;  // 为T0赋初值0xB800, 根据主频计算计时20ms的初值
+  // TL1 = 0x00;
+  T1 = TIME_50_MS;
+  TR1 = ENABLE;  // 启动T1
 
   while (count < 20) {
     if (TF1 == OVERFLOW) {  // T1溢出
       TF1 = NOT_OVERFLOW;   // T1溢出后，清零中断标志
       T1 = 0x4c00;          // 0x4c00
-
+      // 以下是计时20ms 的初值，t20_two = 0xB800
+      // t20 = 2^16 - t20_two
+      // t50 = t20/2 * 5
+      // t50_two = 2^16 - t50 = 19456
+      // TH0 = 0xB8;  // 重新赋初值
+      // TL0 = 0x00;
       ++count;  //计数值自加1
     }
   }
 }
+
 /** 使某一数码管显示某字符
  * @param digit_tube_id 管子的id 0-5 表示 LEDS0-LEDS5
  *                      也可以直接输入寄存器P1 的取值
